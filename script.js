90013de6a21d8b0de97df4f6076e495369afe5e4
zaza110607 <safiramuliza1106@gmail.com>
2026-09-18 18:47:51 +0700

    Add password check functionality in script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+function checkPassword() {
+
+    // =========================
+    // GANTI KODE RAHASIA DI SINI
+    // =========================
+
+    const correctPassword = "piya";
+
+    const password =
+        document.getElementById("password").value;
+
+    const message =
+        document.getElementById("message");
+
+
+    // =========================
+    // CEK PASSWORD
+    // =========================
+
+    if (password.toLowerCase() === correctPassword) {
+
+        message.innerHTML =
+            "🎉 Kode benar! Surprise dimulai... 💙";
+
+        message.style.color = "#16804b";
+
+        setTimeout(function() {
+
+            window.location.href = "quiz.html";
+
+        }, 1500);
+
+    } else {
+
+        message.innerHTML =
+            "🤭 Hmm... kode rahasianya salah! Coba lagi.";
+
+        message.style.color = "#d35b70";
+
+    }
+}
